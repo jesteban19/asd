2ec937425702ddb2a45f89ff9e3b6b6f42801bed
Joseph Esteban <esteban.programador@gmail.com>
2024-06-21 05:00:50 -0500

    Update orm.py

--- database/orm.py
+++ database/orm.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 async def get_session_orm():
     load_dotenv()
-    db_url = f'postgresql://{os.getenv("USER")}:{os.getenv("PWD")}@localhost:5432/{os.getenv("DATABASE")}'
+    db_url = f'postgresql://{os.getenv("USER")}:{os.getenv("PWD")}@127.0.0.1:5432/{os.getenv("DATABASE")}'
     engine = create_engine(db_url)
     session = sessionmaker(bind=engine)
     return session()

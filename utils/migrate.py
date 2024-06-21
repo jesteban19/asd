979861de70640b2541373853b4cdc3df298b2bc9
Joseph Esteban <esteban.programador@gmail.com>
2024-06-21 01:13:58 -0500

    add tools

--- utils/migrate.py
+++ utils/migrate.py
@@ -1,13 +1,25 @@
+import csv
 import os
 import aiofiles
+import asyncpg
+import asyncio
 
-from database import get_db_connection
-from services.MigrationService import MigrationService
+async def get_db_connection():
+    conn = await asyncpg.connect(
+        user="devttecnico22",
+        password="Jyuwh%cwe=",
+        host="localhost",
+        database="devttecnico22"
+    )
+    return conn
 
 
-class CommentsUtil:
-    service = MigrationService()
-    async def migrate(self):
+async def get_db_session():
+    return await get_db_connection()
+
+
+class MigrateTools:
+    async def migrate_comments(self):
         table_name = "comentarios"
         csv_file = os.path.join("resources", "testcomentarios.csv")
         await self.upload_csv_to_db(table_name, csv_file)
@@ -23,13 +35,35 @@ class CommentsUtil:
                     for i in range(0, 9):
                         headers.append(f"columna{i + 1}")
 
-                    await self.service.build(content=content, batch_size=500000, table_name=table_name, headers=headers,
-                                             conn=conn)
+                    await self.build(content=content, batch_size=500000, table_name=table_name, headers=headers,
+                                     conn=conn)
             except Exception as e:
                 raise e
+
+    async def build(self, content, batch_size, table_name, headers, conn):
+        content_with_boom = content.lstrip('\ufeff')
+        rows = content_with_boom.splitlines()
+        reader = csv.reader(rows, delimiter='|')
+        batch = []
+        placeholders = ', '.join(['$' + str(i + 1) for i in range(len(headers))])
+        insert_query = f'INSERT INTO {table_name} ({", ".join(headers)}) VALUES ({placeholders})'
+        for row in reader:
+            if len(row) == len(headers):
+                batch.append(row)
+            if len(batch) >= batch_size:
+                await conn.executemany(insert_query, batch)
+                batch = []
+        if batch:
+            await conn.executemany(insert_query, batch)
 
 
 # executing migrations
 
-comments = CommentsUtil()
-comments.migrate()
+async def runAll():
+    try:
+        comments = MigrateTools()
+        await comments.migrate_comments()
+    except Exception as e:
+        raise e
+
+asyncio.run(runAll())
